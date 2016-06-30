309ef5fc8f22b3467425f9ba8b8336b148b2922a
TJ DeVries <tim.devries@dornerworks.com>
2016-06-30 13:45:17 -0400

    Add basic doc string parsing.
    So far it seems to be reasonably modular. Soon I will start trying
    it on actual functions

--- easy_python_requirements.py
+++ easy_python_requirements.py
@@ -1,14 +1,81 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import json
+
 
 config = {
     'requirement_begin': 'TEST DESCRIPTION BEGIN',
     'requirement_end': 'TEST DESCRIPTION END',
     'requirement_info': 'TEST INFO:',
+    'info_format': ['test_id'],
 }
 
-def parse_doc(docstring):
+
+def trim(docstring):
+    """ From PEP """
+    if not docstring:
+        return ''
+    # Convert tabs to spaces (following the normal Python rules)
+    # and split into a list of lines:
+    lines = docstring.expandtabs().splitlines()
+    # Determine minimum indentation (first line doesn't count):
+    indent = 99
+    for line in lines[1:]:
+        stripped = line.lstrip()
+        if stripped:
+            indent = min(indent, len(line) - len(stripped))
+    # Remove indentation (first line is special):
+    trimmed = [lines[0].strip()]
+    if indent < 99:
+        for line in lines[1:]:
+            trimmed.append(line[indent:].rstrip())
+    # Strip off trailing and leading blank lines:
+    while trimmed and not trimmed[-1]:
+        trimmed.pop()
+    while trimmed and not trimmed[0]:
+        trimmed.pop(0)
+    # Return a single string:
+    return '\n'.join(trimmed)
+
+
+def index_containing_substring(search_list, substring):
+    for index, s in enumerate(search_list):
+        if substring in s:
+            return index
+
+    raise ValueError(search_list.index(substring))
+
+
+def info_line_status(doclist, info_index):
+    """
+    Determine the attributes of the info line
+
+    Args:
+        doclist (list): The docstring -> list
+        info_index (int): The index to begin checking at
+
+    Returns:
+        dict: Information dictionary, specifying important info
+    """
+    info_dict = {}
+    if doclist[info_index] == config['requirement_info']:
+        info_dict['requires_update'] = True
+    else:
+        try:
+            info_json = json.loads(doclist[info_index].split(config['requirement_info'])[1])
+
+            if all(x in info_json.keys() for x in config['info_format']):
+                info_dict['requires_update'] = False
+            else:
+                info_dict['requires_update'] = True
+        except ValueError:
+            info_dict['requires_update'] = True
+
+    return info_dict
+
+
+def parse_doc(docstring: str):
     """
     Parse the requirement and information from a docstring
 
@@ -16,6 +83,32 @@ def parse_doc(docstring):
         docstring (str): the docstring of the test function
 
     Returns:
-        list: [requirement description, requirement info]
+        list: [requirement_description, dictionary with info]
+            requirement_description: None if it could not be found,
+            otherwise it is a string with the description inside of it
+
+            dictionary_with_info: A dictionary filled with information
+            about the current test specification.
     """
-    pass
+    doclist = trim(docstring).split('\n')
+
+    try:
+        requirement_begin = index_containing_substring(doclist, config['requirement_begin'])
+    except ValueError:
+        return [None, {'requires_update': False}]
+
+    try:
+        requirement_end = index_containing_substring(doclist, config['requirement_end'])
+    except ValueError:
+        return [None, {'requires_update': False}]
+
+    requirement_description = '\n'.join(doclist[requirement_begin + 1:requirement_end])
+
+    try:
+        requirement_info = index_containing_substring(doclist, config['requirement_info'])
+    except ValueError:
+        return [requirement_description, {'requires_update': True}]
+
+    info_dict = info_line_status(doclist, requirement_info)
+
+    return [requirement_description, info_dict]
--- test/test_parse_doc.py
+++ test/test_parse_doc.py
@@ -1,15 +1,66 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from easy_python_requirements import parse_doc
+
+
 class TestParseDoc:
     def test_parse_doc_no_result(self):
-        pass
+        docstring = """
+        This has got nothing important in it.
+        Move along
+        """
+        assert(parse_doc(docstring) == [None,
+                                        {'requires_update': False}
+                                        ])
 
     def test_parse_doc_empty_info(self):
-        pass
+        docstring = """
+        This one has no information
+        TEST DESCRIPTION BEGIN
+        But it does have some description
+        TEST DESCRIPTION END
+        """
+        assert(parse_doc(docstring) == ['But it does have some description',
+                                        {'requires_update': True}
+                                        ])
 
     def test_parse_doc_existing_info(self):
-        pass
+        docstring = """
+        This one has info
+        TEST INFO: at least something is here
+        TEST DESCRIPTION BEGIN
+        Also with description
+        TEST DESCRIPTION END
+        """
+        desc, requirement_info = parse_doc(docstring)
+
+        assert(desc == 'Also with description')
+        assert(requirement_info['requires_update'] is True)
+
+        docstring = """
+        This one has almost valid info
+        TEST INFO: {"bad_id": 6}
+        TEST DESCRIPTION BEGIN
+        This is bad
+        TEST DESCRIPTION END
+        """
+        desc, requirement_info = parse_doc(docstring)
+
+        assert(desc == 'This is bad')
+        assert(requirement_info['requires_update'] is True)
+
+        docstring = """
+        This one has valid info
+        TEST INFO: {"test_id": 6}
+        TEST DESCRIPTION BEGIN
+        This is good
+        TEST DESCRIPTION END
+        """
+        desc, requirement_info = parse_doc(docstring)
+
+        assert(desc == 'This is good')
+        assert(requirement_info['requires_update'] is False)
 
     def test_parse_doc_no_begin(self):
         pass
